05636aff6a7b7084bab232102c23275496f17576
Bril Mikhail <Kolabrod@mail.ru>
2020-03-25 16:32:33 +0300

    Updates

---
+++ precache-manifest.605f33de15f3c8d09e054f37a494d042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c080bddcc33e4a378a2b46ff40b2417",
+    "url": "/quiz-react/index.html"
+  },
+  {
+    "revision": "df1f97c7dcaf42b7170f",
+    "url": "/quiz-react/static/css/main.e360e7f1.chunk.css"
+  },
+  {
+    "revision": "5e5cd39de54d4bf0d661",
+    "url": "/quiz-react/static/js/2.f11a0937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-react/static/js/2.f11a0937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1f97c7dcaf42b7170f",
+    "url": "/quiz-react/static/js/main.8db00ae8.chunk.js"
+  },
+  {
+    "revision": "b31d57e86daf849e5697",
+    "url": "/quiz-react/static/js/runtime-main.d0d0962d.js"
+  }
+]);
